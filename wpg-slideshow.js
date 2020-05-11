a3735a91f10cead8b6249c44f0a2f9117ac183bc
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-05-11 16:27:03 +0200

    first commit

---
+++ wpg-slideshow.js
@@ -0,0 +1,187 @@
+/**
+ * Start slideshow, when DOM is fully loaded.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    var slideshow = new WPGSlideshow("wpg-slideshow");
+    slideshow.activate();
+});
+/**
+ * Class to enable Slideshow functionality for WP Gallery Blocks.
+ */
+var WPGSlideshow = /** @class */ (function () {
+    /**
+     * @constructor
+     * Default constructor.
+     *
+     * @param {string} - Freely choosable class name of slideshow container.
+     */
+    function WPGSlideshow(className) {
+        this.ssClassName = className ? className : "wpg-slideshow";
+        this.slideshows = new Array();
+        // Get slideshow HTML object array and activate slideshow
+        this.slideshowsNodeList = document.querySelectorAll("." + this.ssClassName);
+    }
+    /**
+     * Injects slideshow markup and adds functionality for Slideshows.
+     */
+    WPGSlideshow.prototype.activate = function () {
+        var _this = this;
+        if (this.slideshowsNodeList) {
+            var ssID_1 = 0; // Identifier for slideshow HTML elements
+            // Inject markup for every slideshow
+            this.slideshowsNodeList.forEach(function (slideshow) {
+                // Build ID for slideshow html container
+                var ssIDText = "wpg-slideshow-" + ssID_1;
+                slideshow.setAttribute("id", ssIDText);
+                // Get all WP-Block-Gallery Elements
+                var containerNodeList = slideshow.querySelectorAll(".blocks-gallery-grid");
+                // Run trough gallery containers
+                containerNodeList.forEach(function (container) {
+                    // Add class to slideshow container
+                    container.classList.add("wpg-slideshow__container");
+                    // Get all blocks gallery item (container for images)
+                    var items = container.querySelectorAll(".blocks-gallery-item");
+                    var itemID = 0;
+                    // Add id, wpg slideshow classes and styles to items and imgs
+                    items.forEach(function (item) {
+                        itemID++;
+                        item.id = "wpg-slide-" + itemID; // First id has value of 1
+                        item.classList.add("wpg-slideshow__item");
+                        item.style.cssText = "margin-bottom: 0!important"; // Overwrite default margin of wp-block-gallery
+                        item.style.display = itemID === 1 ? "block" : "none"; // Show first img, hide rest
+                        // Get all imgs and add identification data to markup
+                        var imgs = item.querySelectorAll("img");
+                        imgs.forEach(function (img) {
+                            img.classList.add("wpg-slideshow__img");
+                            img.setAttribute("data-ssid", itemID.toString()); // Add slideshow id to image
+                            img.setAttribute("data-loaded", "false"); // True if image has loaded
+                        });
+                    });
+                    // Build and add naivgation buttons to slideshow container
+                    _this.addNavButton(container, ssID_1, "prev");
+                    _this.addNavButton(container, ssID_1, "next");
+                    _this.addNavButtonEvents(ssID_1);
+                    // Remember slideshow data
+                    var ssObj = new Slideshow(ssID_1, itemID);
+                    _this.slideshows.push(ssObj);
+                });
+            });
+        }
+    };
+    /**
+     * Add navigations buttons (next/prev) to slideshow container.
+     *
+     * @param {any} - Slideshow container html element.
+     * @param {number} - Slideshow id.
+     * @param {string} - Button type ("prev" or "next").
+     */
+    WPGSlideshow.prototype.addNavButton = function (parent, btnID, btnType) {
+        // Assign '<' for prev and '>' for next
+        var icon = btnType === "prev" ? "&#10094" : "&#10095";
+        parent.insertAdjacentHTML("beforebegin", "<a id= 'wpg-slideshow-" + btnID.toString() + "-" + btnType + "' class='wpg-slideshow__button wpg-slideshow__button--" + btnType + "'\n    data-ssid=" + btnID.toString() + ">" + icon + "</a>");
+        /* Build markup for button
+        parent.innerHTML += `<a id= 'wpg-slideshow-${btnID.toString()}-${btnType}' class='wpg-slideshow__button wpg-slideshow__button--${btnType}'
+        data-ssid=${btnID.toString()}>${icon}</a>`; // data-ssid is used for eventhandling in changeSlides()*/
+    };
+    /**
+     * Add event handler for navigation buttons.
+     * @param {number} - Button (=slideshow) ID.
+     */
+    WPGSlideshow.prototype.addNavButtonEvents = function (btnID) {
+        var _this = this;
+        // Get previous button
+        var prevBtn = document.getElementById("wpg-slideshow-" + btnID.toString() + "-prev");
+        // Change slide to previous on click
+        prevBtn.addEventListener("click", function (e) {
+            _this.changeSlides(prevBtn, -1);
+        });
+        // Get next button
+        var nextBtn = document.getElementById("wpg-slideshow-" + btnID.toString() + "-next");
+        // Change slide to next on click
+        nextBtn.addEventListener("click", function (e) {
+            _this.changeSlides(nextBtn, 1);
+        });
+    };
+    /**
+     * Changes slides to next or previous.
+     *
+     * @param {Object} - Clicked button as html element.
+     * @param {Number} - Value which changes index of slideshow (-1/+1).
+     */
+    WPGSlideshow.prototype.changeSlides = function (btn, changeValue) {
+        // Get changing slideshow
+        var id = btn.getAttribute("data-ssid");
+        var slideshow = this.slideshows[id];
+        // Hide current slide
+        var currentSlide = document.getElementById("wpg-slide-" + slideshow.index);
+        currentSlide.style.display = "none";
+        console.log("%c Changing slides:", "color: green; font-weight: bold;");
+        console.log("Current Slide ID: " + slideshow.index);
+        console.log("Change Value: " + changeValue);
+        // Show the newly selected slide
+        if (slideshow.index == 1 && changeValue == -1) {
+            // Previous button pressed, first slide is shown -> set to last slide
+            slideshow.setIndexToLast();
+        }
+        else if (slideshow.lastSlideIsShown() && changeValue == 1) {
+            // Next button pressed, last slide is shown -> set to first slide
+            slideshow.setIndexToFirst();
+        }
+        else {
+            // Next or previous button pressed, change slide accordingly
+            slideshow.changeIndex(changeValue);
+        }
+        var newSlide = document.getElementById("wpg-slide-" + slideshow.index);
+        newSlide.style.display = "block";
+        // Put changed slideshow back on array
+        this.slideshows[id] = slideshow;
+        console.log("Next Slide ID: " + slideshow.index);
+        console.log("Image Count: " + slideshow.imageCount);
+    };
+    return WPGSlideshow;
+}());
+var Slideshow = /** @class */ (function () {
+    /**
+     * @constructor
+     * Default constructor.
+     *
+     * @param {number} - Slideshow id.
+     * @param {number} - Slideshow image count (or number of slides).
+     */
+    function Slideshow(id, imageCount) {
+        this._startIndex = 1;
+        this.id = id;
+        this.index = 1;
+        this.imageCount = imageCount;
+    }
+    /**
+     * Given number is added to index.
+     */
+    Slideshow.prototype.changeIndex = function (index) {
+        this.index += index;
+    };
+    /**
+     * Index is set to last position (number of images).
+     */
+    Slideshow.prototype.setIndexToLast = function () {
+        this.index = this.imageCount;
+    };
+    /**
+     * Index is set to first position (1).
+     */
+    Slideshow.prototype.setIndexToFirst = function () {
+        this.index = this._startIndex;
+    };
+    /**
+     * Checks if current index is last slide.
+     *
+     * @return {Boolean} - True if index is last slide.
+     */
+    Slideshow.prototype.lastSlideIsShown = function () {
+        if (this.index === this.imageCount)
+            return true;
+        else
+            return false;
+    };
+    return Slideshow;
+}());
---
+++ wpg-slideshow.ts
@@ -0,0 +1,230 @@
+/**
+ * Start slideshow, when DOM is fully loaded.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  let slideshow = new WPGSlideshow("wpg-slideshow");
+  slideshow.activate();
+});
+
+/**
+ * Class to enable Slideshow functionality for WP Gallery Blocks.
+ */
+class WPGSlideshow {
+  ssClassName: string; // Class name of slideshow.
+  slideshows: Slideshow[]; // Array of Slideshow class objects.
+  slideshowsNodeList: NodeListOf<Element>; // NodeList of slideshows on current page.
+
+  /**
+   * @constructor
+   * Default constructor.
+   *
+   * @param {string} - Freely choosable class name of slideshow container.
+   */
+  constructor(className: string) {
+    this.ssClassName = className ? className : "wpg-slideshow";
+    this.slideshows = new Array();
+
+    // Get slideshow HTML object array and activate slideshow
+    this.slideshowsNodeList = document.querySelectorAll("." + this.ssClassName);
+  }
+
+  /**
+   * Injects slideshow markup and adds functionality for Slideshows.
+   */
+  activate() {
+    if (this.slideshowsNodeList) {
+      let ssID: number = 0; // Identifier for slideshow HTML elements
+
+      // Inject markup for every slideshow
+      this.slideshowsNodeList.forEach((slideshow) => {
+        // Build ID for slideshow html container
+        let ssIDText: string = "wpg-slideshow-" + ssID;
+        slideshow.setAttribute("id", ssIDText);
+
+        // Get all WP-Block-Gallery Elements
+        let containerNodeList: NodeListOf<Element> = slideshow.querySelectorAll(
+          ".blocks-gallery-grid"
+        );
+
+        // Run trough gallery containers
+        containerNodeList.forEach((container) => {
+          // Add class to slideshow container
+          container.classList.add("wpg-slideshow__container");
+
+          // Get all blocks gallery item (container for images)
+          let items: NodeListOf<any> = container.querySelectorAll(
+            ".blocks-gallery-item"
+          );
+          let itemID: number = 0;
+
+          // Add id, wpg slideshow classes and styles to items and imgs
+          items.forEach((item) => {
+            itemID++;
+            item.id = "wpg-slide-" + itemID; // First id has value of 1
+            item.classList.add("wpg-slideshow__item");
+            item.style.cssText = "margin-bottom: 0!important"; // Overwrite default margin of wp-block-gallery
+            item.style.display = itemID === 1 ? "block" : "none"; // Show first img, hide rest
+
+            // Get all imgs and add identification data to markup
+            let imgs: NodeListOf<Element> = item.querySelectorAll("img");
+            imgs.forEach((img) => {
+              img.classList.add("wpg-slideshow__img");
+              img.setAttribute("data-ssid", itemID.toString()); // Add slideshow id to image
+              img.setAttribute("data-loaded", "false"); // True if image has loaded
+            });
+          });
+
+          // Build and add naivgation buttons to slideshow container
+          this.addNavButton(container, ssID, "prev");
+          this.addNavButton(container, ssID, "next");
+          this.addNavButtonEvents(ssID);
+
+          // Remember slideshow data
+          let ssObj: Slideshow = new Slideshow(ssID, itemID);
+          this.slideshows.push(ssObj);
+        });
+      });
+    }
+  }
+
+  /**
+   * Add navigations buttons (next/prev) to slideshow container.
+   *
+   * @param {any} - Slideshow container html element.
+   * @param {number} - Slideshow id.
+   * @param {string} - Button type ("prev" or "next").
+   */
+  addNavButton(parent: any, btnID: number, btnType: string) {
+    // Assign '<' for prev and '>' for next
+    let icon = btnType === "prev" ? "&#10094" : "&#10095";
+
+    parent.insertAdjacentHTML(
+      "beforebegin",
+      `<a id= 'wpg-slideshow-${btnID.toString()}-${btnType}' class='wpg-slideshow__button wpg-slideshow__button--${btnType}'
+    data-ssid=${btnID.toString()}>${icon}</a>`
+    );
+
+    /* Build markup for button
+    parent.innerHTML += `<a id= 'wpg-slideshow-${btnID.toString()}-${btnType}' class='wpg-slideshow__button wpg-slideshow__button--${btnType}'
+    data-ssid=${btnID.toString()}>${icon}</a>`; // data-ssid is used for eventhandling in changeSlides()*/
+  }
+
+  /**
+   * Add event handler for navigation buttons.
+   * @param {number} - Button (=slideshow) ID.
+   */
+  addNavButtonEvents(btnID: number) {
+    // Get previous button
+    let prevBtn: any = document.getElementById(
+      `wpg-slideshow-${btnID.toString()}-prev`
+    );
+
+    // Change slide to previous on click
+    prevBtn.addEventListener("click", (e: any) => {
+      this.changeSlides(prevBtn, -1);
+    });
+
+    // Get next button
+    let nextBtn: any = document.getElementById(
+      `wpg-slideshow-${btnID.toString()}-next`
+    );
+
+    // Change slide to next on click
+    nextBtn.addEventListener("click", (e: any) => {
+      this.changeSlides(nextBtn, 1);
+    });
+  }
+
+  /**
+   * Changes slides to next or previous.
+   *
+   * @param {Object} - Clicked button as html element.
+   * @param {Number} - Value which changes index of slideshow (-1/+1).
+   */
+  changeSlides(btn: any, changeValue: number) {
+    // Get changing slideshow
+    let id: number = btn.getAttribute("data-ssid");
+    let slideshow: Slideshow = this.slideshows[id];
+
+    // Hide current slide
+    let currentSlide: any = document.getElementById(
+      `wpg-slide-${slideshow.index}`
+    );
+    currentSlide.style.display = "none";
+
+    console.log("%c Changing slides:", "color: green; font-weight: bold;");
+    console.log(`Current Slide ID: ${slideshow.index}`);
+    console.log(`Change Value: ${changeValue}`);
+
+    // Show the newly selected slide
+    if (slideshow.index == 1 && changeValue == -1) {
+      // Previous button pressed, first slide is shown -> set to last slide
+      slideshow.setIndexToLast();
+    } else if (slideshow.lastSlideIsShown() && changeValue == 1) {
+      // Next button pressed, last slide is shown -> set to first slide
+      slideshow.setIndexToFirst();
+    } else {
+      // Next or previous button pressed, change slide accordingly
+      slideshow.changeIndex(changeValue);
+    }
+    let newSlide = document.getElementById(`wpg-slide-${slideshow.index}`);
+    newSlide.style.display = "block";
+
+    // Put changed slideshow back on array
+    this.slideshows[id] = slideshow;
+
+    console.log(`Next Slide ID: ${slideshow.index}`);
+    console.log(`Image Count: ${slideshow.imageCount}`);
+  }
+}
+
+class Slideshow {
+  public id: number;
+  public index: number;
+  public imageCount: number;
+  private _startIndex: number = 1;
+
+  /**
+   * @constructor
+   * Default constructor.
+   *
+   * @param {number} - Slideshow id.
+   * @param {number} - Slideshow image count (or number of slides).
+   */
+  constructor(id: number, imageCount: number) {
+    this.id = id;
+    this.index = 1;
+    this.imageCount = imageCount;
+  }
+
+  /**
+   * Given number is added to index.
+   */
+  changeIndex(index: number) {
+    this.index += index;
+  }
+
+  /**
+   * Index is set to last position (number of images).
+   */
+  setIndexToLast() {
+    this.index = this.imageCount;
+  }
+
+  /**
+   * Index is set to first position (1).
+   */
+  setIndexToFirst() {
+    this.index = this._startIndex;
+  }
+
+  /**
+   * Checks if current index is last slide.
+   *
+   * @return {Boolean} - True if index is last slide.
+   */
+  lastSlideIsShown(): boolean {
+    if (this.index === this.imageCount) return true;
+    else return false;
+  }
+}
